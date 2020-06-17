e401542a050331ae65f3e133deca2d33b9dbee9a
Moez B <moezbajwa@hotmail.com>
2020-06-16 23:57:56 -0400

    edited home command

--- src/mod/commands/HomeCommand.java
+++ src/mod/commands/HomeCommand.java
@@ -1,6 +1,7 @@
 package mod.commands;
 
 import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
 import org.bukkit.Location;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
@@ -19,7 +20,7 @@ public class HomeCommand implements CommandExecutor {
 	
 	public boolean onCommand(CommandSender sender, Command cmd, String str, String[] args) {
 		((Player)sender).teleport(new Location(Bukkit.getWorld("world"),77.621,30.0,-28.485));
-		this.plugin.getServer().broadcastMessage("You have been teleported back to da ferda house!");
+		this.plugin.getServer().broadcastMessage(ChatColor.RED + ((Player)sender).getName().toString() + " has been teleported back to da ferda house!");
 		return false;
 	}
 
